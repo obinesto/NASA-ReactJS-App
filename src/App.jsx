452da27a363598ecaea9e36c8ae74242660bb7c3
Cyprian Obi <obicyprian180@gmail.com>
2024-11-12 04:07:52 +0100

    update loader

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import Main from "./components/Main";
 import SideBar from "./components/SideBar";
 import Footer from "./components/Footer";
-// import Loader from "./components/Loader";
+import Loader from "./components/Loader";
 import fallbackImage from "./assets/ssc2008-10b1.png";
 
 const fallbackData = {
@@ -78,11 +78,11 @@ function App() {
       {data ? (
         <Main data={data} />
       ) : (
-        <div className="loadingState">
-          <i className="fa-solid fa-gear fa-2xl"></i>
-          <h2>Loading...</h2>
-        </div>
-        // <Loader/>
+        // <div className="loadingState">
+        //   <i className="fa-solid fa-gear fa-2xl"></i>
+        //   <h2>Loading...</h2>
+        // </div>
+        <Loader/>
       )}
       {showModal && (
         <SideBar data={data} handleToggleModal={handleToggleModal} />
